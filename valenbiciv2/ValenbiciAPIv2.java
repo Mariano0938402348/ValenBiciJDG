923b8ffd43934e9699e027a7186ef78f9eaeb035
Mariano0938402348 <marianojacquet38@gmail.com>
2025-05-13 14:24:44 +0200

    Delete valenbiciv2 directory

--- valenbiciv2/ValenbiciAPIv2.java
+++
@@ -1,9 +0,0 @@
-
-package es.gva.edu.iesjuandegaray.valenbiciv2;
-
-public class ValenbiciAPIv2 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
